ec476e8426b454587091b8bae397e312c18066dd
Tom Anderson <tomacorp@gmail.com>
2014-08-01 00:18:11 -0700

    Added Solver class

--- therm.py
+++ therm.py
@@ -37,7 +37,226 @@ class Mesh:
     self.field = np.zeros((self.width+1,self.height+1,5), dtype = 'double')
     self.xr, self.yr= np.mgrid[:self.width+1, :self.height+1]
 
+class Matls:
+  def __init__(self):
+    self.copper= 10
+    self.fr4cond= 0.1
     
+# Find the number of rows in the sparse matrix
+def countElts(mesh):
+  elts = 0;
+  for x in range(1, mesh.width-1):
+    for y in range(1, mesh.height-1):
+      elts = elts + 1
+  return elts
+
+# This can scale by using a PNG input
+def defineproblem(lyr, mesh):
+
+  mesh.field[:, :, lyr.heat]  = 0.0
+  mesh.field[:, :, lyr.resis] = 0.1
+  mesh.field[:, :, lyr.iso]   = 0.0
+  mesh.field[:, :, lyr.deg]   = 23
+  mesh.field[:, :, lyr.flux]  = 0.0
+  
+  # Heat source
+  hsx= 0.5
+  hsy= 0.5
+  hswidth= 0.25
+  hsheight= 0.25
+  srcl= round(mesh.width*(hsx-hswidth*0.5))
+  srcr= round(mesh.width*(hsx+hswidth*0.5))
+  srct= round(mesh.height*(hsy-hsheight*0.5))
+  srcb= round(mesh.height*(hsy+hsheight*0.5))
+  mesh.field[srcl:srcr, srct:srcb, lyr.heat] = 5.0
+  mesh.field[srcl:srcr, srct:srcb, lyr.resis] = 10.0
+  
+  # Boundary conditions
+  mesh.field[0, 0:mesh.height-1, lyr.iso] = 1.0
+  mesh.field[mesh.width-1:mesh.width, 0:mesh.height-1, lyr.iso] = 1.0
+  mesh.field[0:mesh.width-1, 0, lyr.iso] = 1.0
+  mesh.field[0:mesh.width-1, mesh.height-1, lyr.iso] = 1.0
+  
+  # Thermal conductors
+  condwidth= 0.05
+  cond1l= round(mesh.width*hsx - mesh.width*condwidth*0.5)
+  cond1r= round(mesh.width*hsx + mesh.width*condwidth*0.5)
+  cond1t= round(mesh.height*hsy - mesh.height*condwidth*0.5)
+  cond1b= round(mesh.height*hsy + mesh.height*condwidth*0.5)
+  mesh.field[0:mesh.width, cond1t:cond1b, lyr.resis] = 10.0
+  mesh.field[cond1l:cond1r, 0:mesh.height, lyr.resis] = 10.0
+
+# This can scale by using a PNG output
+def plotsolution(lyr, mesh):
+  z1= mesh.field[:, :, lyr.flux];
+  z2= mesh.field[:, :, lyr.deg];
+
+  plt.figure(1)
+  plt.subplot(1,1,1)
+  plt.axes(aspect=1)
+  quad= plt.pcolormesh(mesh.xr, mesh.yr, z1)
+  plt.colorbar()
+  plt.draw()
+  
+  plt.figure(2)
+  plt.subplot(1,1,1)
+  plt.axes(aspect=1)
+  quad2= plt.pcolormesh(mesh.xr, mesh.yr, z2)
+  plt.colorbar()
+  plt.draw()
+  plt.show()
+
+def gsitersolve(lyr, mesh, monitor):
+  finished = 0
+  iter = 0
+  while ((iter < 1200) and (finished == 0)):
+    monitor.powerout = 0
+    monitor.powerin = 0
+    delta= simstep(iter, lyr, mesh, monitor)
+    print iter, delta, monitor.maxtemp, monitor.powerin, monitor.powerout
+    powerbalance= (monitor.powerout - monitor.powerin)/(monitor.powerout+monitor.powerin)
+    if ((iter > 10) and (abs(powerbalance) < .005)):
+      finished = 1
+    iter = iter + 1
+
+class Solver:
+
+  def __init__(self):
+    self.Comm              = Epetra.PyComm()
+    # elts= countElts(mesh)
+    self.NumGlobalElements = 5
+    self.Map               = Epetra.Map(self.NumGlobalElements, 0, self.Comm)
+    self.A                 = Epetra.CrsMatrix(Epetra.Copy, self.Map, 0)
+    self.NumMyElements     = self.Map.NumMyElements()
+    self.MyGlobalElements  = self.Map.MyGlobalElements()
+    self.b = Epetra.Vector(self.Map)
+    print "MyGlobalElements =", self.MyGlobalElements
+
+  def loadMatrix(self):
+    R1 = 10.0
+    R2 = 10.0
+    R3 = 15.0
+    R4 = 15.0
+    R5 = 5.0
+
+    # define the communicator (Serial or parallel, depending on your configure
+    # line), then initialize a distributed matrix of size 4. The matrix is empty,
+    # `0' means to allocate for 0 elements on each row (better estimates make the
+    # code faster). `NumMyElements' is the number of rows that are locally hosted 
+    # by the calling processor; `MyGlobalElements' is the global ID of locally 
+    # hosted rows.
+    R6 = 30.0
+
+    # A is the problem matrix
+    # Modified nodal analysis
+    # http://www.swarthmore.edu/NatSci/echeeve1/Ref/mna/MNA2.html
+    # node 0
+    self.A[0, 0] = 1/R1
+    # node 1
+    self.A[1, 1] = 1/R1 + 1/R2 + 1/R3
+    # node 2
+    self.A[2, 2] = 1/R3 + 1/R4 + 1/R5
+    # node 3
+    self.A[3, 3] = 1/R5 + 1/R6
+    # Common node impedances
+    self.A[0, 1] = -1/R1
+    self.A[1, 0] = -1/R1
+    self.A[1, 2] = -1/R3
+    self.A[2, 1] = -1/R3
+    self.A[2, 3] = -1/R5
+    self.A[3, 2] = -1/R5
+    # Independent voltage source into node 0
+    self.A[0, 4] = 1
+    self.A[4, 0] = 1
+
+    # b is the RHS, which are current sources for injected heat and voltage sources for 
+    #   boundar condition.
+    self.b[0] = 0
+    self.b[1] = 0
+    self.b[2] = 0
+    # This is the only term for a 1A current source injected into node 3
+    self.b[3] = 1
+    # This is the 10V voltage source going into node 0.
+    # Current going in to the arrow of this source is in the solution as x[4]
+    # In this example, the current flows in the direction of the arrow on the current source,
+    # so the solution to the current is negative.
+    self.b[4] = 10
+
+  def solveMatrix(self):
+    # x are the unknowns to be solved.
+    self.x = Epetra.Vector(self.Map)
+  
+    self.A.FillComplete()
+
+    solver = AztecOO.AztecOO(self.A, self.x, self.b)
+
+    # This loads x with the solution to the problem
+    solver.Iterate(1550, 1e-5)
+
+    self.Comm.Barrier()
+
+    for i in self.MyGlobalElements:
+      print "PE%d: %d %e" % (self.Comm.MyPID(), i, self.x[i])
+  
+    # synchronize processors
+    self.Comm.Barrier()
+
+    if self.Comm.MyPID() == 0: print "End Result: TEST PASSED"
+
+
+# This will become a pytrilinos solver
+def simstep(iter, lyr, mesh, monitor):
+  maxdelta= 0;
+  mesh.field[:, :, lyr.flux] = 0.0
+  for x in range(1, mesh.width-1):
+    for y in range(1, mesh.height-1):
+      if (mesh.field[x , y, lyr.iso] != 1.0):
+        resisl= mesh.field[x - 1, y, lyr.resis]
+        resisr= mesh.field[x + 1, y, lyr.resis]
+        resisu= mesh.field[x, y - 1, lyr.resis]
+        resisd= mesh.field[x, y + 1, lyr.resis]
+        tresis= resisl+resisr+resisu+resisd
+        degl= mesh.field[x - 1, y, lyr.deg]
+        degr= mesh.field[x + 1, y, lyr.deg]
+        degu= mesh.field[x, y - 1, lyr.deg]
+        degd= mesh.field[x, y + 1, lyr.deg]
+        
+        if (tresis > 0):
+          prevdeg = mesh.field[x, y, lyr.deg]
+          powerincell = mesh.field[x, y, lyr.heat]
+          monitor.powerin = monitor.powerin + powerincell
+          newdeg = (powerincell + ( degl*resisl + degr*resisr + degu*resisu + degd*resisd ))/tresis
+
+          poweroutcell = 0.0
+          fluxl= (prevdeg - degl) * resisl
+          fluxr= (prevdeg - degr) * resisr
+          fluxu= (prevdeg - degu) * resisu
+          fluxd= (prevdeg - degd) * resisd
+
+          if (mesh.field[x - 1 , y, lyr.iso] == 1.0):
+            poweroutcell = poweroutcell + fluxl
+          if (mesh.field[x + 1 , y, lyr.iso] == 1.0):
+            poweroutcell = poweroutcell + fluxr
+          if (mesh.field[x , y - 1, lyr.iso] == 1.0):
+            poweroutcell = poweroutcell + fluxu
+          if (mesh.field[x , y + 1, lyr.iso] == 1.0):
+            poweroutcell = poweroutcell + fluxd
+
+          mesh.field[x - 1 , y, lyr.flux] += abs(fluxl)
+          mesh.field[x + 1 , y, lyr.flux] += abs(fluxr)
+          mesh.field[x, y - 1, lyr.flux] += abs(fluxu)
+          mesh.field[x, y + 1, lyr.flux] += abs(fluxd)
+          
+          mesh.field[x, y, lyr.deg]= newdeg;
+
+          if (maxdelta < abs(prevdeg - newdeg)):
+            maxdelta = abs(prevdeg - newdeg)
+          if (monitor.maxtemp < newdeg):
+            monitor.maxtemp = newdeg
+
+          monitor.powerout = monitor.powerout + poweroutcell
+  return maxdelta
+
 def aztecsolve(lyr, mesh, monitor):
   # define the communicator (Serial or parallel, depending on your configure
   # line), then initialize a distributed matrix of size 4. The matrix is empty,
@@ -119,135 +338,19 @@ def aztecsolve(lyr, mesh, monitor):
 
   if Comm.MyPID() == 0: print "End Result: TEST PASSED"
 
-# This can scale by using a PNG input
-def defineproblem(lyr, mesh):
-
-  mesh.field[:, :, lyr.heat]  = 0.0
-  mesh.field[:, :, lyr.resis] = 0.1
-  mesh.field[:, :, lyr.iso]   = 0.0
-  mesh.field[:, :, lyr.deg]   = 23
-  mesh.field[:, :, lyr.flux]  = 0.0
-  
-  # Heat source
-  hsx= 0.5
-  hsy= 0.5
-  hswidth= 0.25
-  hsheight= 0.25
-  srcl= round(mesh.width*(hsx-hswidth*0.5))
-  srcr= round(mesh.width*(hsx+hswidth*0.5))
-  srct= round(mesh.height*(hsy-hsheight*0.5))
-  srcb= round(mesh.height*(hsy+hsheight*0.5))
-  mesh.field[srcl:srcr, srct:srcb, lyr.heat] = 5.0
-  mesh.field[srcl:srcr, srct:srcb, lyr.resis] = 10.0
-  
-  # Boundary conditions
-  mesh.field[0, 0:mesh.height-1, lyr.iso] = 1.0
-  mesh.field[mesh.width-1:mesh.width, 0:mesh.height-1, lyr.iso] = 1.0
-  mesh.field[0:mesh.width-1, 0, lyr.iso] = 1.0
-  mesh.field[0:mesh.width-1, mesh.height-1, lyr.iso] = 1.0
-  
-  # Thermal conductors
-  condwidth= 0.05
-  cond1l= round(mesh.width*hsx - mesh.width*condwidth*0.5)
-  cond1r= round(mesh.width*hsx + mesh.width*condwidth*0.5)
-  cond1t= round(mesh.height*hsy - mesh.height*condwidth*0.5)
-  cond1b= round(mesh.height*hsy + mesh.height*condwidth*0.5)
-  mesh.field[0:mesh.width, cond1t:cond1b, lyr.resis] = 10.0
-  mesh.field[cond1l:cond1r, 0:mesh.height, lyr.resis] = 10.0
-
-# This can scale by using a PNG output
-def plotsolution(lyr, mesh):
-  z1= mesh.field[:, :, lyr.flux];
-  z2= mesh.field[:, :, lyr.deg];
-
-  plt.figure(1)
-  plt.subplot(1,1,1)
-  plt.axes(aspect=1)
-  quad= plt.pcolormesh(mesh.xr, mesh.yr, z1)
-  plt.colorbar()
-  plt.draw()
-  
-  plt.figure(2)
-  plt.subplot(1,1,1)
-  plt.axes(aspect=1)
-  quad2= plt.pcolormesh(mesh.xr, mesh.yr, z2)
-  plt.colorbar()
-  plt.draw()
-  plt.show()
-
-def gsitersolve(lyr, mesh, monitor):
-  finished = 0
-  iter = 0
-  while ((iter < 1200) and (finished == 0)):
-    monitor.powerout = 0
-    monitor.powerin = 0
-    delta= simstep(iter, lyr, mesh, monitor)
-    print iter, delta, monitor.maxtemp, monitor.powerin, monitor.powerout
-    powerbalance= (monitor.powerout - monitor.powerin)/(monitor.powerout+monitor.powerin)
-    if ((iter > 10) and (abs(powerbalance) < .005)):
-      finished = 1
-    iter = iter + 1
-
-# This will become a pytrilinos solver
-def simstep(iter, lyr, mesh, monitor):
-  maxdelta= 0;
-  mesh.field[:, :, lyr.flux] = 0.0
-  for x in range(1, mesh.width-1):
-    for y in range(1, mesh.height-1):
-      if (mesh.field[x , y, lyr.iso] != 1.0):
-        resisl= mesh.field[x - 1, y, lyr.resis]
-        resisr= mesh.field[x + 1, y, lyr.resis]
-        resisu= mesh.field[x, y - 1, lyr.resis]
-        resisd= mesh.field[x, y + 1, lyr.resis]
-        tresis= resisl+resisr+resisu+resisd
-        degl= mesh.field[x - 1, y, lyr.deg]
-        degr= mesh.field[x + 1, y, lyr.deg]
-        degu= mesh.field[x, y - 1, lyr.deg]
-        degd= mesh.field[x, y + 1, lyr.deg]
-        
-        if (tresis > 0):
-          prevdeg = mesh.field[x, y, lyr.deg]
-          powerincell = mesh.field[x, y, lyr.heat]
-          monitor.powerin = monitor.powerin + powerincell
-          newdeg = (powerincell + ( degl*resisl + degr*resisr + degu*resisu + degd*resisd ))/tresis
-
-          poweroutcell = 0.0
-          fluxl= (prevdeg - degl) * resisl
-          fluxr= (prevdeg - degr) * resisr
-          fluxu= (prevdeg - degu) * resisu
-          fluxd= (prevdeg - degd) * resisd
-
-          if (mesh.field[x - 1 , y, lyr.iso] == 1.0):
-            poweroutcell = poweroutcell + fluxl
-          if (mesh.field[x + 1 , y, lyr.iso] == 1.0):
-            poweroutcell = poweroutcell + fluxr
-          if (mesh.field[x , y - 1, lyr.iso] == 1.0):
-            poweroutcell = poweroutcell + fluxu
-          if (mesh.field[x , y + 1, lyr.iso] == 1.0):
-            poweroutcell = poweroutcell + fluxd
-
-          mesh.field[x - 1 , y, lyr.flux] += abs(fluxl)
-          mesh.field[x + 1 , y, lyr.flux] += abs(fluxr)
-          mesh.field[x, y - 1, lyr.flux] += abs(fluxu)
-          mesh.field[x, y + 1, lyr.flux] += abs(fluxd)
-          
-          mesh.field[x, y, lyr.deg]= newdeg;
-
-          if (maxdelta < abs(prevdeg - newdeg)):
-            maxdelta = abs(prevdeg - newdeg)
-          if (monitor.maxtemp < newdeg):
-            monitor.maxtemp = newdeg
-
-          monitor.powerout = monitor.powerout + poweroutcell
-  return maxdelta
 
 lyr = Layers()
 monitor = Monitors()
 mesh = Mesh(25, 20)
+matls = Matls()
 
 defineproblem(lyr, mesh)
 gsitersolve(lyr, mesh, monitor)
 plotsolution(lyr, mesh)
+
+solv = Solver()
+solv.loadMatrix()
+solv.solveMatrix()
 
 aztecsolve(lyr, mesh, monitor)
 
